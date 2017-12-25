7db74eae0382b2ea958308123da739a450baa034
Remy Betus <remy.betus@akeneo.com>
2017-12-25 11:14:39 +0100

    1 measure every 15 min

--- arduino/read_measure_from_arduino.py
+++ arduino/read_measure_from_arduino.py
@@ -19,16 +19,16 @@ sensorPort = args.port
 print('reading from serial port %s...' % sensorPort)
 
 device = serial.Serial(sensorPort, 9600)
-measures = [-127, -127, -127, -127];
+measures = [0, 0, 0, 0];
 while True:
     line = device.readline().strip()
     measure = line.split(';')
     if len(measure) > 1:
-     
+
         sql = "INSERT INTO `measure` (`sensor_id`, `value`,  `date`) VALUES (%s, %s, NOW())"
-        measure_type,sensor_id,value = measure  
+        measure_type,sensor_id,value = measure
         value = format(float(value), '.1f');
-        if measures[int(sensor_id) - 1] != value:
+        if measures[int(sensor_id) - 1] == 0:
             cursor.execute(sql, (sensor_id, value))
             print('inserting value', value, 'for sensor with ID ', sensor_id)
             db.commit()
@@ -36,10 +36,13 @@ while True:
                 print('last insert id', cursor.lastrowid)
             else:
                 print('last insert id not found')
+        # sensors send messages every 30sec. here is to ignore all but one in 15min.
+        elif measures[int(sensor_id) - 1] == 30:
+            measures[int(sensor_id) - 1] = 0;
         else:
             print('ignored same previous value', value, 'for sensor with ID ', sensor_id)
-        measures[int(sensor_id) - 1] = value;
-                
+        measures[int(sensor_id) - 1] += 1;
+
     else:
          print("Connected")
 db.close()
