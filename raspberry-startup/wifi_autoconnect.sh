421ff4bbb9dfef6a8841bc34d97ab9aac00810ec
Remy Betus <remy.betus@akeneo.com>
2019-12-24 15:49:35 +0100

    Add instructions for wifi autoconnect

--- raspberry-startup/wifi_autoconnect.sh
+++ raspberry-startup/wifi_autoconnect.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
-# auto reconnect to wifi
-# 
- 
-IFACE="wlan0"
-NET="192.168.0.254"
- 
-if ifconfig $IFACE &>/dev/null && ifconfig $IFACE | grep -q "inet"; then
-   echo "Network is up!"
-elif ifconfig $IFACE &>/dev/null && iwlist $IFACE scan|grep $NET &>/dev/null; then
-   echo "Network connection down! WIFI AP available, connecting..."
-   ifup --force $IFACE
+
+# Auto reconnects to a WiFi AP
+# Based on https://www.raspberrypi.org/forums/viewtopic.php?f=91&t=16054&start=25#p656295
+#
+# Set up cron, as root:
+# crontab -e
+# */5 * * * * /home/pi/meteo/raspberry-startup/wifi_autoconnect.sh > /var/logs/wifi_connection.log 2>&1
+#
+# Side note: interesting read on WPA protection, to avoid having your WiFi password in plain text
+# https://www.raspberrypi.org/documentation/configuration/wireless/wireless-cli.md
+#
+# 1. wpa_passphrase "wifi name"
+# 2. Type in actual password
+# 3. Replace content in /etc/wpa_supplicant/wpa_supplicant.conf
+
+# Fixed IP in router setting, allowing external access
+raspberyIp="192.168.0.253"
+APName=$(cat /etc/wpa_supplicant/wpa_supplicant.conf | grep ssid | cut -d'"' -f2)
+
+if ifconfig wlan0 | grep -q "inet ${raspberyIp}"; then
+   echo "$(date) - Network is up!"
+elif iwlist wlan0 scan|grep ${APName} &>/dev/null; then
+   echo "$(date) - Network connection down! ${APName} is available, connecting..."
+   ip link set wlan0 up
+else
+   echo "$(date) - Network is down or ${APName} is not available."
 fi
