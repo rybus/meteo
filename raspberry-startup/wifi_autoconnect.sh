7d89a14ef1f534d4e29c5622d50b7aa31cd1490b
Remy Betus <remy.betus@akeneo.com>
2019-12-24 15:58:10 +0100

    Fix path for ifconfig and iwlist

--- raspberry-startup/wifi_autoconnect.sh
+++ raspberry-startup/wifi_autoconnect.sh
@@ -3,7 +3,7 @@
 # Auto reconnects to a WiFi AP
 # Based on https://www.raspberrypi.org/forums/viewtopic.php?f=91&t=16054&start=25#p656295
 #
-# Set up cron, as root:
+# Set up cron to run every 5 minutes, as root:
 # crontab -e
 # */5 * * * * /home/pi/meteo/raspberry-startup/wifi_autoconnect.sh > /var/logs/wifi_connection.log 2>&1
 #
@@ -18,9 +18,9 @@
 raspberyIp="192.168.0.253"
 APName=$(cat /etc/wpa_supplicant/wpa_supplicant.conf | grep ssid | cut -d'"' -f2)
 
-if ifconfig wlan0 | grep -q "inet ${raspberyIp}"; then
+if /sbin/ifconfig wlan0 | grep -q "inet ${raspberyIp}"; then
    echo "$(date) - Network is up!"
-elif iwlist wlan0 scan|grep ${APName} &>/dev/null; then
+elif /sbin/iwlist wlan0 scan|grep ${APName} &>/dev/null; then
    echo "$(date) - Network connection down! ${APName} is available, connecting..."
    ip link set wlan0 up
 else
