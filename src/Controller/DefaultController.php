2aa3fdb203ae7cee653a04edf900540a4b3588a4
Remy Betus <3594268+rybus@users.noreply.github.com>
2020-01-05 13:09:39 +0100

    Fix some deprecation warnings

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,12 +5,12 @@ namespace App\Controller;
 use App\Entity\Sensor;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
+class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
@@ -27,7 +27,7 @@ class DefaultController extends Controller
             ];
         }
 
-        return $this->render('@App/Meteo/home.html.twig', ['last_measures' => $lastSensorMeasure]);
+        return $this->render('home.html.twig', ['last_measures' => $lastSensorMeasure]);
     }
 
     /**
@@ -100,7 +100,7 @@ class DefaultController extends Controller
         );
 
         return $this->render(
-            'App:Meteo:history.html.twig',
+            'history.html.twig',
             [
                 'sensor' => $sensor,
                 'start' => $start->getTimestamp(),
